fe911242db751a8c5499c5ba3526f058ae33d2fe
Chris Storz <chris.storz@harness.io>
2023-04-25 13:02:04 -0700

    first flag

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
-from flask import Flask
+import os
+from featureflags.client import CfClient
+from featureflags.evaluations.auth_target import Target
+from flask import Flask, render_template
 app = Flask(__name__)
+
+api_key = os.environ.get('ff_key')
+cf = CfClient(api_key)
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    target = Target(identifier="user1", name="user1")
+    new_ui = cf.bool_variation("new_ui", target, False)
+    if not new_ui:
+        homepage = 'Hello, World!'
+    else:
+        homepage = render_template('hello.html', name="World")
+    return homepage
 
 
 if __name__ == '__main__':
